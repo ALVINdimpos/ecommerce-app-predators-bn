e22ffdb17a338b0e1c14173d3afe2cef4c38fd8b
ALVINdimpos <49131269+ALVINdimpos@users.noreply.github.com>
2023-06-27 15:50:28 +0200

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -5,20 +5,23 @@ import {
   isSeller,
   isBuyer,
 } from '../middleware/roles.js';
-import { getAllNotifications, markNotificationAsRead, deleteNotification } from '../controller/notificationController.js';
+import { getAllNotifications, markNotificationAsRead, deleteNotification ,markAllAsRead} from '../controller/notificationController.js';
 
 const router = Router();
 router.get('/buyer/notification',isBuyer, getAllNotifications);
 router.put('/buyer/:notificationId', isBuyer, markNotificationAsRead);
 router.delete('/buyer/:notificationId', isBuyer, deleteNotification);
+router.put('/buyer/mark-all-as-read', isBuyer, markAllAsRead)
 //seller
 router.get('/seller/notification',isSeller, getAllNotifications);
 router.put('/seller/:notificationId', isSeller, markNotificationAsRead);
 router.delete('/seller/:notificationId', isSeller, deleteNotification);
+router.put('/seller/mark-all-as-read', isSeller, markAllAsRead)
+
 //admin
 router.get('/admin/notification',isAdmin, getAllNotifications);
 router.put('/admin/:notificationId', isAdmin, markNotificationAsRead);
 router.delete('/admin/:notificationId', isAdmin, deleteNotification);
+router.put('/admin/mark-all-as-read', isAdmin, markAllAsRead)
 
 export default router;
-
