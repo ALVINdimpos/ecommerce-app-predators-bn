b3b2a6fb62e8dbd512ca055ac5d8f1b24abb004d
ALVINdimpos <49131269+ALVINdimpos@users.noreply.github.com>
2023-06-27 22:07:19 +0200

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,27 +1,16 @@
 /* eslint-disable */
 import { Router } from 'express';
 import {
-  isAdmin,
-  isSeller,
-  isBuyer,
+  isLoggedIn
 } from '../middleware/roles.js';
 import { getAllNotifications, markNotificationAsRead, deleteNotification ,markAllAsRead} from '../controller/notificationController.js';
 
 const router = Router();
-router.get('/buyer/notification',isBuyer, getAllNotifications);
-router.put('/buyer/:notificationId', isBuyer, markNotificationAsRead);
-router.delete('/buyer/:notificationId', isBuyer, deleteNotification);
-router.put('/buyer/mark-all-as-read', isBuyer, markAllAsRead)
-//seller
-router.get('/seller/notification',isSeller, getAllNotifications);
-router.put('/seller/:notificationId', isSeller, markNotificationAsRead);
-router.delete('/seller/:notificationId', isSeller, deleteNotification);
-router.put('/seller/mark-all-as-read', isSeller, markAllAsRead)
+router.get('/notification',isLoggedIn, getAllNotifications);
+router.put('/notification/mark-all-as-read', isLoggedIn, markAllAsRead);
+router.put('/notification/:notificationId', isLoggedIn, markNotificationAsRead);
+router.delete('/notification/:notificationId', isLoggedIn, deleteNotification);
 
-//admin
-router.get('/admin/notification',isAdmin, getAllNotifications);
-router.put('/admin/:notificationId', isAdmin, markNotificationAsRead);
-router.delete('/admin/:notificationId', isAdmin, deleteNotification);
-router.put('/admin/mark-all-as-read', isAdmin, markAllAsRead)
+
 
 export default router;
